43ff221a2e921451b754c80e634d3e25cbf5c607
kneeknee97 <aainee97@gmail.com>
2024-11-25 09:50:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelector('.pre-save-form').addEventListener('submit', function(event) {
+  event.preventDefault();
+  
+  // Here you can submit the form data
+  alert('Thank you for pre-saving!');
+});
